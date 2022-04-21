c624f858a8e6c3eda6bd43675f6d301485182e93
Kruziy <yanolfirenko9@gmail.com>
2022-04-21 14:29:36 +0200

    ya hz

--- ConsoleApplication4/ConsoleApplication4.cpp
+++ ConsoleApplication4/ConsoleApplication4.cpp
@@ -17,5 +17,22 @@ int main()
 		if (str[i] == letter) chs++;
 	}
 	cout << "Letter " << letter << " vstrechayestya v slove " << chs << " raz" << endl;
+	cout << "Vvedite slovo: ";
+	cin.ignore();
+	getline(cin, str);
+	int pr;
+	cout << "Vvedite nomer bukvi kotoruyu hotite udalit: ";
+	cin >> pr;
+	string n_str = str;
+	for (int i = 0, j = 0; str[i] != '\0'; i++)
+	{
+		if (i != pr - 1)
+		{
+			n_str[j] = str[i];
+			j++;
+		}
+	}
+	n_str[n_str.size() - 1] = '\0';
+	cout << n_str;
 }
 
