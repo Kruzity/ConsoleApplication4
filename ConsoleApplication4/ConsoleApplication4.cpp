e71e0daa2fe753cf61ef3fa2cf192e0138516f32
Kruziy <yanolfirenko9@gmail.com>
2022-04-21 13:58:17 +0200

    Написать программу, которая заменяет все символы точки "." в строке, введенной пользователем, на символы восклицательного знака "!".

--- ConsoleApplication4/ConsoleApplication4.cpp
+++ ConsoleApplication4/ConsoleApplication4.cpp
@@ -1,12 +1,18 @@
 #include <iostream>
+#include<string>
 using namespace std;
 
-void print()
-{
-    cout << "Hello world!" << endl;
-}
+
 int main()
 {
-    print();
+	string str;
+	cout << "Vvedite slovo: ";
+	cin.ignore();
+	getline(cin, str);
+	for (int i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] == '.') str[i] = '!';
+	}
+	cout << "\n" << str;
 }
 
