76e55bcf25edcc71914fbd7d0e04423c850c623c
Kruziy <yanolfirenko9@gmail.com>
2022-04-21 14:14:05 +0200

    Пользователь вводит строку символов и искомый символ, посчитать сколько раз он встречается в строке.

--- ConsoleApplication4/ConsoleApplication4.cpp
+++ ConsoleApplication4/ConsoleApplication4.cpp
@@ -1,7 +1,21 @@
 #include <iostream>
+#include<string>
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    string str;
+	int chs = 0;
+	cout << "Vvedite slovo: ";
+	cin.ignore();
+	getline(cin, str);
+	char letter;
+	cout << "enter looking letter: ";
+	cin >> letter;
+	for (int i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] == letter) chs++;
+	}
+	cout << "Letter " << letter << " vstrechayestya v slove " << chs << " raz" << endl;
 }
 
