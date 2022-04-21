5ffc620480ec409da0f0a459af683529efa6464c
Kruziy <yanolfirenko9@gmail.com>
2022-04-21 14:42:19 +0200

    Написать функцию, которая удаляет из строки символ с заданным номером.

--- ConsoleApplication4/ConsoleApplication4.cpp
+++ ConsoleApplication4/ConsoleApplication4.cpp
@@ -1,7 +1,27 @@
 #include <iostream>
+#include<string>
+using namespace std;
+
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	string str;
+	cout << "Vvedite slovo: ";
+	cin.ignore();
+	getline(cin, str);
+	int pr;
+	cout << "Vvedite nomer bukvi kotoruyu hotite udalit: ";
+	cin >> pr;
+	string n_str = str;
+	for (int i = 0, j = 0; str[i] != '\0'; i++)
+	{
+		if (i != pr - 1)
+		{
+			n_str[j] = str[i];
+			j++;
+		}
+	}
+	n_str[n_str.size() - 1] = '\0';
+	cout << n_str;
 }
 
